eda935280fc52baba0f3d1ffe5ee2257521ee921
Sunmock Yang <sunmock@live.ca>
2015-08-19 22:59:12 -0400

    Added client side code to use sockets to chat
    - Now everything pretty much works!

--- Client-side/WebSocketChat.js
+++ Client-side/WebSocketChat.js
@@ -1,10 +1,33 @@
-function WebSocketChat(username, chatWindow) {
+function WebSocketChat(username, chatWindow, url) {
+
 	this.username = username;
 	this.chatWindow = chatWindow;
+	this.url = url;
+	
+	if (username == null || username == ""){
+		this.displayMessage("CLIENT", "YOU NEED A USERNAME. REFRESH THE PAGE.");
+		return;
+	}
+
+	this.displayMessage("CLIENT", "Connecting to: '" + this.url + "'");
+
+	this.websocket = new WebSocket(this.url);
+	this.websocket.onmessage = this.onReceive.bind(this);
+	this.websocket.onopen = this.onOpen.bind(this);
+	this.websocket.onclose = this.onClose.bind(this);
 }
 
+WebSocketChat.prototype.onOpen = function(event) {
+	this.displayMessage("SERVER", "Connection made.");
+};
+
+WebSocketChat.prototype.onClose = function(event) {
+	this.displayMessage("SERVER", "Connection closed.")
+};
+
 WebSocketChat.prototype.onReceive = function(rawPacket) {
-	var packet = JSON.parse(rawPacket);
+	console.log(rawPacket.data)
+	var packet = JSON.parse(rawPacket.data);
 	this.displayMessage(packet.username, packet.message);
 };
 
@@ -15,6 +38,8 @@ WebSocketChat.prototype.sendMessage = function(message) {
 	};
 
 	this.displayMessage(this.username, message);
+
+	this.websocket.send(JSON.stringify(packet));
 };
 
 WebSocketChat.prototype.displayMessage = function(username, message) {
